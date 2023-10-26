51077cb5c5e3689c39f383b0e9fecf6c1bf8926c
MUHAMMEDZAMILC <mzofficial049@gmail.com>
2023-10-26 09:42:28 +0530

    .

--- custom_button/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ custom_button/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/user/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user/MZ/custom_button"
+export "FLUTTER_APPLICATION_PATH=/Users/user/MZ/flutter_functionality/custom_button"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
